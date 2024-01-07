c95e7c8f015452e1ed6a3f735047ca8fa0cfcfab
Nick Palmer <nick@palmr.co.uk>
2024-01-07 23:38:19 +0000

    palmr v3 overhaul

--- src/main/java/dev/morling/onebrc/CalculateAverage_palmr.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_palmr.java
@@ -17,7 +17,10 @@ package dev.morling.onebrc;
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
 import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
@@ -26,20 +29,19 @@ import java.util.stream.StreamSupport;
 
 public class CalculateAverage_palmr {
     private static final String FILE = "./measurements.txt";
-    private static final int CHUNK_SIZE = 1024 * 1024 * 10; // Trial and error showed ~10MB to be a good size on our machine
-    private static final int STATION_NAME_BUFFER_SIZE = 128;
     private static final int THREAD_COUNT = Math.min(8, Runtime.getRuntime().availableProcessors());
-    private static final char SEPARATOR_CHAR = ';';
-    private static final char END_OF_RECORD = '\n';
-    private static final char MINUS_CHAR = '-';
-    private static final char DECIMAL_POINT_CHAR = '.';
+    public static final boolean PARALLEL = true;
+    private static final byte SEPARATOR_CHAR = ';';
+    private static final byte END_OF_RECORD = '\n';
+    private static final byte MINUS_CHAR = '-';
+    private static final byte DECIMAL_POINT_CHAR = '.';
 
     public static void main(String[] args) throws IOException {
 
         final var file = new RandomAccessFile(FILE, "r");
         final var channel = file.getChannel();
 
-        final TreeMap<String, MeasurementAggregator> results = StreamSupport.stream(ThreadChunk.chunk(file, THREAD_COUNT), true)
+        final TreeMap<String, MeasurementAggregator> results = StreamSupport.stream(ThreadChunk.chunk(file, THREAD_COUNT), PARALLEL)
                 .map(chunk -> parseChunk(chunk, channel))
                 .flatMap(bakm -> bakm.getAsUnorderedList().stream())
                 .collect(Collectors.toMap(m -> new String(m.stationNameBytes, StandardCharsets.UTF_8), m -> m, MeasurementAggregator::merge, TreeMap::new));
@@ -49,17 +51,16 @@ public class CalculateAverage_palmr {
     private record ThreadChunk(long startPoint, long endPoint, long size) {
         public static Spliterator<CalculateAverage_palmr.ThreadChunk> chunk(final RandomAccessFile file, final int chunkCount) throws IOException {
             final var fileSize = file.length();
-            final var idealChunkSize = Math.max(CHUNK_SIZE, fileSize / THREAD_COUNT);
+            final var idealChunkSize = fileSize / THREAD_COUNT;
             final var chunks = new CalculateAverage_palmr.ThreadChunk[chunkCount];
 
             var validChunks = 0;
             var startPoint = 0L;
             for (int i = 0; i < chunkCount; i++) {
                 var endPoint = Math.min(startPoint + idealChunkSize, fileSize);
-                if (startPoint + idealChunkSize < fileSize)
-                {
+                if (startPoint + idealChunkSize < fileSize) {
                     file.seek(endPoint);
-                    while (endPoint++ < fileSize && file.readByte() != END_OF_RECORD) {
+                    while (file.readByte() != END_OF_RECORD && endPoint++ < fileSize) {
                         Thread.onSpinWait();
                     }
                 }
@@ -67,18 +68,15 @@ public class CalculateAverage_palmr {
                 final var actualSize = endPoint - startPoint;
                 if (actualSize > 1) {
                     chunks[i] = new CalculateAverage_palmr.ThreadChunk(startPoint, endPoint, actualSize);
-                    startPoint += actualSize;
+                    startPoint += actualSize + 1;
                     validChunks++;
-                }
-                else {
+                } else {
                     break;
                 }
             }
 
             return Spliterators.spliterator(chunks, 0, validChunks,
-                    Spliterator.ORDERED |
                             Spliterator.DISTINCT |
-                            Spliterator.SORTED |
                             Spliterator.NONNULL |
                             Spliterator.IMMUTABLE |
                             Spliterator.CONCURRENT
@@ -86,91 +84,76 @@ public class CalculateAverage_palmr {
         }
     }
 
-    private static ByteArrayKeyedMap parseChunk(ThreadChunk chunk, FileChannel channel) {
-        final var state = new State();
-
-        var offset = chunk.startPoint;
-        while (offset < chunk.endPoint) {
-            parseData(channel, state, offset, Math.min(CHUNK_SIZE, chunk.endPoint - offset));
-            offset += CHUNK_SIZE;
-        }
-
-        return state.aggregators;
-    }
-
-    private static void parseData(final FileChannel channel,
-                                  final State state,
-                                  final long offset,
-                                  final long bufferSize) {
-        final ByteBuffer byteBuffer;
+    private static ByteArrayKeyedMap parseChunk(final ThreadChunk chunk, final FileChannel channel) {
+        final MappedByteBuffer byteBuffer;
         try {
-            byteBuffer = channel.map(FileChannel.MapMode.READ_ONLY, offset, bufferSize);
-
-            while (byteBuffer.hasRemaining()) {
-                final var currentChar = byteBuffer.get();
-
-                if (currentChar == SEPARATOR_CHAR) {
-                    state.parsingValue = true;
-                }
-                else if (currentChar == END_OF_RECORD) {
-                    if (state.stationPointerEnd != 0) {
-                        final var value = state.measurementValue * state.exponent;
-
-                        MeasurementAggregator aggregator = state.aggregators.computeIfAbsent(state.stationBuffer, state.stationPointerEnd, state.signedHashCode);
-                        aggregator.count++;
-                        aggregator.min = Math.min(aggregator.min, value);
-                        aggregator.max = Math.max(aggregator.max, value);
-                        aggregator.sum += value;
-                    }
-
-                    // reset
-                    state.reset();
-                }
-                else {
-                    if (!state.parsingValue) {
-                        state.stationBuffer[state.stationPointerEnd++] = currentChar;
-                        state.signedHashCode = 31 * state.signedHashCode + (currentChar & 0xff);
-                    }
-                    else {
-                        if (currentChar == MINUS_CHAR) {
-                            state.exponent = -0.1;
-                        }
-                        else if (currentChar != DECIMAL_POINT_CHAR) {
-                            state.measurementValue = state.measurementValue * 10 + (currentChar - '0');
-                        }
-                    }
-                }
-            }
+            byteBuffer = channel.map(FileChannel.MapMode.READ_ONLY, chunk.startPoint, chunk.size);
         }
         catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
+        var aggregators = new ByteArrayKeyedMap();
 
-    private static final class State {
-        ByteArrayKeyedMap aggregators = new ByteArrayKeyedMap();
-        boolean parsingValue = false;
-        byte[] stationBuffer = new byte[STATION_NAME_BUFFER_SIZE];
-        int signedHashCode = 0;
-        int stationPointerEnd = 0;
-        double measurementValue = 0;
-        double exponent = 0.1;
+        var currentChar = -1;
+        var ptr = 0;
+        while (ptr < chunk.size) {
+            if (byteBuffer.get(ptr) == END_OF_RECORD) {
+                break;
+            }
+            var stationNamePointerStart = ptr;
+            var stationNameLength = 0;
+            var stationNamePointerHashCode = 0;
+            var measurementValue = 0;
 
-        public void reset() {
-            parsingValue = false;
-            signedHashCode = 0;
-            stationPointerEnd = 0;
-            measurementValue = 0;
-            exponent = 0.1;
+            while (ptr < chunk.endPoint && (currentChar = byteBuffer.get(ptr++)) != SEPARATOR_CHAR) {
+                stationNameLength++;
+                stationNamePointerHashCode = 31 * stationNamePointerHashCode + (currentChar & 0xFF);
+            }
+
+            var d1 = byteBuffer.get(ptr++);
+            var d2 = byteBuffer.get(ptr++);
+            var d3 = byteBuffer.get(ptr++);
+            if (d1 == MINUS_CHAR) {
+                if (d3 == DECIMAL_POINT_CHAR) {
+                    // -x.x
+                    // 1234
+                    measurementValue = -((d2 - '0') * 10 + (byteBuffer.get(ptr++) - '0'));
+                }
+                else {
+                    // -xx.x
+                    // 12345
+                    measurementValue = -((d2 - '0') * 100 + (d3 - '0') * 10 + (byteBuffer.get(ptr += 2) - '0'));
+                }
+            }
+            else {
+                if (d2 == DECIMAL_POINT_CHAR) {
+                    // x.x
+                    // 123
+                    measurementValue = ((d1 - '0') * 10 + (d3 - '0'));
+                }
+                else {
+                    // xx.x
+                    // 1234
+                    measurementValue = ((d1 - '0') * 100 + (d2 - '0') * 10 + (byteBuffer.get(ptr++) - '0'));
+                }
+            }
+
+            MeasurementAggregator aggregator = aggregators.computeIfAbsent(byteBuffer, stationNamePointerStart, stationNameLength, stationNamePointerHashCode);
+            aggregator.count++;
+            aggregator.min = Math.min(aggregator.min, measurementValue);
+            aggregator.max = Math.max(aggregator.max, measurementValue);
+            aggregator.sum += measurementValue;
         }
+
+        return aggregators;
     }
 
     private static class MeasurementAggregator {
         final byte[] stationNameBytes;
         final int stationNameHashCode;
-        private double min = Double.POSITIVE_INFINITY;
-        private double max = Double.NEGATIVE_INFINITY;
-        private double sum;
+        private long min = Long.MAX_VALUE;
+        private long max = Long.MIN_VALUE;
+        private long sum;
         private long count;
 
         public MeasurementAggregator(final byte[] stationNameBytes, final int stationNameHashCode) {
@@ -179,7 +162,7 @@ public class CalculateAverage_palmr {
         }
 
         public String toString() {
-            return STR."\{round(min)}/\{round(sum / count)}/\{round(max)}";
+            return round(min * 0.1) + "/" + round((sum * 0.1) / count) + "/" + round(max * 0.1);
         }
 
         private double round(final double value) {
@@ -207,21 +190,33 @@ public class CalculateAverage_palmr {
         private final MeasurementAggregator[] buckets = new MeasurementAggregator[BUCKET_COUNT + 1];
         private final List<MeasurementAggregator> compactUnorderedBuckets = new ArrayList<>(413);
 
-        public MeasurementAggregator computeIfAbsent(final byte[] key, final int keyLength, final int keyHashCode) {
-            var index = keyHashCode & BUCKET_COUNT;
+        public MeasurementAggregator computeIfAbsent(final ByteBuffer byteBuffer,
+                                                     final int stationNamePointerStart,
+                                                     final int stationNameLength,
+                                                     final int stationNamePointerHashCode) {
+            var index = stationNamePointerHashCode & BUCKET_COUNT;
 
-            while (true) {
+            outer: while (true) {
                 MeasurementAggregator maybe = buckets[index];
                 if (maybe != null) {
-                    if (Arrays.equals(key, 0, keyLength, maybe.stationNameBytes, 0, maybe.stationNameBytes.length)) {
-                        return maybe;
+                    if (maybe.stationNameBytes.length != stationNameLength) {
+                        index++;
+                        index &= BUCKET_COUNT;
+                        continue;
                     }
-                    index++;
-                    index &= BUCKET_COUNT;
+                    for (var i = 0; i < stationNameLength; i++) {
+                        if (maybe.stationNameBytes[i] != byteBuffer.get(stationNamePointerStart + i)) {
+                            index++;
+                            index &= BUCKET_COUNT;
+                            continue outer;
+                        }
+                    }
+                    return maybe;
                 }
                 else {
-                    final var copiedKey = Arrays.copyOf(key, keyLength);
-                    MeasurementAggregator measurementAggregator = new MeasurementAggregator(copiedKey, keyHashCode);
+                    var copiedKey = new byte[stationNameLength];
+                    byteBuffer.get(stationNamePointerStart, copiedKey);
+                    MeasurementAggregator measurementAggregator = new MeasurementAggregator(copiedKey, stationNamePointerHashCode);
                     buckets[index] = measurementAggregator;
                     compactUnorderedBuckets.add(measurementAggregator);
                     return measurementAggregator;
